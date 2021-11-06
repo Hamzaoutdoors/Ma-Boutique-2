3a118f3380935aa892c5561f4de82f1ac5d6bdd1
Hamza <hamzaoutdoors@gmail.com>
2021-11-07 00:26:40 +0100

    feat : Added slide to the home page

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import { useState } from 'react';
 import styled from 'styled-components';
+import { sliderItems } from '../data';
 
 const Container = styled.div`
     width: 100%;   
     height: 100vh;
     display: flex;
     position: relative;
+    overflow-x: hidden;
 `;
 
 const Arrow = styled.div`
@@ -24,10 +27,14 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `;
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transition: all 1.5s ease-in-out;
+    transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -35,6 +42,7 @@ const Slide = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
@@ -71,27 +79,39 @@ const Button = styled.button`
    cursor: pointer;
 `;
 
-const Slider = () => (
-  <Container>
-    <Arrow direction="left">
-      <ArrowLeftOutlined />
-    </Arrow>
-    <Wrapper>
-      <Slide>
-        <ImgContainer>
-          <Image src="./assets/slider/First_image.png" alt="slide_image" />
-        </ImgContainer>
-        <InfoContainer>
-          <Title>SUMMER SALE</Title>
-          <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
-          <Button>SHOW NOW</Button>
-        </InfoContainer>
-      </Slide>
-    </Wrapper>
-    <Arrow direction="right">
-      <ArrowRightOutlined />
-    </Arrow>
-  </Container>
-);
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === 'left') {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick('left')}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide key={item.id} bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} alt={item.title} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick('right')}>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
 
 export default Slider;
