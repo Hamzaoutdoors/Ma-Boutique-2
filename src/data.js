ceabe95954987903d6eeb15bdfcd19dbd4c8f034
Hamza <hamzaoutdoors@gmail.com>
2021-11-07 00:26:14 +0100

    feat : first items data

---
+++ src/data.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/prefer-default-export */
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: './assets/slider/First_image.png',
+    title: 'SUMMER SALE',
+    desc: 'DON\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: './assets/slider/second_image.png',
+    title: 'AUTUMN COLLECTION',
+    desc: 'DON\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: './assets/slider/third_image.png',
+    title: 'LOUNGEWEAR LOVE',
+    desc: 'DON\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS',
+    bg: '4c575f',
+  },
+];
