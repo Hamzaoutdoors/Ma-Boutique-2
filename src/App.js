ffc980b5a69e725c81d1cf162633beba8d62a83b
Hamza <hamzaoutdoors@gmail.com>
2021-12-12 15:47:55 +0100

    chore: Add image to the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import ProductDetail from './pages/Product';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 const App = () => <div><Home /></div>;
 
